6b778f9861ca81ecf419397481d76aec4e6853da
Franco Emanuel Pasante <emanuel@serverhp.dec>
2023-10-11 12:48:20 -0300

    add model BD

--- filtros/back/index.php
+++ filtros/back/index.php
@@ -1,11 +1,10 @@
 <?php
-
+/*
     function find( $departament, $theme){
         
         $path = 'N:\Informatica\FRANCO';
         $data = file_get_contents("../utils/survey.json");
         $filters = json_decode($data, true);
-        //$real_path = $path . $censo . "\" . $departament . "\" . $filters['modules'][$modulo]['tema'][][1] . $filters['modules'][$modulo]['id'] . "-FR-" . $departament . ".xls" ;
         echo var_dump($filters);
     }
     $year = 2010;
@@ -30,4 +29,26 @@ foreach ($valuIndex as $modules) {
     }
 }
 
-?>
+*/
+$year = 2010;
+$path = 'N:\Informatica\FRANCO';
+$censo = '\Censo ' . $year . '-cuadros por muni frac y radio';
+$themes = "Caracteristicas Habitacionales";
+$data = file_get_contents("C:\\xampp\\htdocs\\front\\utils\\survey.json");
+$filters = json_decode($data, true);
+$valuIndex = $filters['modules'];
+
+$dataDepartaments = file_get_contents("C:\\xampp\\htdocs\\front\\utils\\departament.json");
+$departaments = json_decode($dataDepartaments, true);
+
+foreach ($departaments['departaments'] as $departament) {
+    foreach ($valuIndex as $modules) {
+        foreach ($modules['tema'] as $themeModule) {
+            foreach ($themeModule['array'] as $quadro) {
+                $real_path = $path . $censo . "\\" . $departament['value'] . "\\" .
+                    $quadro . $modules['id'] . "-FR-" . $departament['value'] . ".xls";
+                echo "<p>$real_path</p>";
+            }
+        }
+    }
+}
