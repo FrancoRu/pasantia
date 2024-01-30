5327e01ae98ad88b74c0b3810c080e6681afb26b
FrancoRu <francoruggeri19@gmail.com>
2024-01-30 11:56:37 -0300

    deleted files

--- sdt/controller/buscador/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Cache-Control: max-age=31536000');
-require_once __DIR__ . '/php/views/layout/header/header.php';
-require_once __DIR__ . '/php/views/form/form.php';
-require_once __DIR__ . '/php/views/table/table.php';
-require_once __DIR__ . '/php/views/layout/footer/footer.php';
